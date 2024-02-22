e9d4acf28a80b87893dfbd95f5137d1533cfc568
karim Mohamed <karimdesouki147@gmail.com>
2024-02-22 11:53:21 +0200

    form Validation Lab

---
+++ Form validation/Form.php
@@ -0,0 +1,95 @@
+<?php require_once("config.php");
+$name="";
+$email="";
+$message="";
+$ErrorMsgName ="";
+$ErrorMsgEmail = "";
+$ErrorMsg = "";
+
+if (isset($_POST["submit"])){
+    $flag = 0;
+    $name = isset($_POST["name"])? $_POST["name"] : "";
+    $email = isset($_POST["email"])? $_POST["email"] : "";
+    $message = isset($_POST["message"])? $_POST["message"] : "";
+    if (empty($name) || strlen($name) > Maxlength){
+        $ErrorMsgName = "Error: Name is required and must be less than " . Maxlength . " characters.";
+        $flag = 1;
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $ErrorMsgEmail = "Error: is not a valid email address";
+        $flag = 1;
+    }
+    
+    if (empty($message) || strlen($message) > Msgmax){
+        $ErrorMsg = "Error: Message is required and must be less than " . Msgmax . " characters.";
+        $flag = 1;
+    }
+    
+    if($flag === 0){
+        echo "<b>Thanks for contacting Us</b>";
+        echo '<br>';
+        echo "Name : $name";
+        echo '<br>';
+        echo "Email : $email";
+        echo '<br>';
+        echo "message : $message";
+        exit;
+    }
+    
+}
+?>
+<html>
+    <head>
+        <title> contact form </title>
+
+
+    </head>
+    
+    <body>
+        <h3> Contact Form </h3>
+        <div id="after_submit">
+            
+        </div>
+        <form id="contact_form" action="test.php" method="POST" enctype="multipart/form-data">
+
+            <div class="row">
+                <label class="required" for="name">Your name:</label><br />
+                <input id="name" class="input" name="name" type="text" value="<?php echo "$name" ?>" size="30" /><br />
+            </div>
+            <?php     
+            if (!empty($ErrorMsgName)) {
+            echo "$ErrorMsgName";
+            }
+            ?>
+            <div class="row">
+                <label class="required" for="email">Your email:</label><br />
+                <input id="email" class="input" name="email" type="text" value="<?php echo "$email" ?>" size="30" /><br />   
+            </div>
+            <?php     
+            if (!empty($ErrorMsgEmail)) {
+            echo "$ErrorMsgEmail";
+            }
+            ?>
+            <div class="row">
+                <label class="required" for="message">Your message:</label><br />
+                <textarea id="message" class="input" name="message" rows="7" value="<?php echo "$message" ?>" cols="30"></textarea><br />  
+            </div>
+            <?php     
+            if (!empty($ErrorMsg)) {
+            echo "$ErrorMsg";
+            echo '<br>';
+            }
+            ?>
+            <input id="submit" name="submit" type="submit" value="Send email" />
+            <input id="clear" name="clear" type="reset" value="clear form" />
+            
+            
+        </form>
+        <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+        </script>
+    </body>
+
+</html>
---
+++ Form validation/config.php
@@ -0,0 +1,9 @@
+<?php
+define("Maxlength",100);
+define("Minlength",10);
+define("Msgmax",255);
+
+
+
+
+?>
